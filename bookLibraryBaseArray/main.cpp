90d426944f1b1d5f9d1e02690adc4b301df1bf43
Tianbin Long <47530574+Longtainbin@users.noreply.github.com>
2020-02-24 18:57:53 +0800

    上传源文件

---
+++ bookLibraryBaseArray/main.cpp
@@ -0,0 +1,44 @@
+﻿// HelloWorld.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include<iostream>
+#include <string>
+#include<ctime>
+//#include"Mydelay.h"
+#include<fstream>
+#include "Mybook.h"
+
+using namespace std;
+int main() 
+{ 
+   Mybook bookLib;
+   bookLib.bookNum=bookLib.creatBookLib();
+   
+   // string matchBook[2]={"JAVA实手册",bookLib.bookArray[3].bookName};
+   // int num=2;
+
+   cout<<"原始数据：\n";
+   bookLib.showBookLib();
+  // cout<<"排序后数据：\n";
+  // bookLib.sortBookLib();
+  // cout<<endl<<"提价后数据\n"<<"原始平均价"<<bookLib.modPrice()<<endl;
+   // cout<<"逆序排列"<<endl;
+   // bookLib.resverSortBookLib();
+    //bookLib.findExpenBook();
+   // bookLib.showBookLib();
+ // cout<<"寻找书籍"<<endl;
+  //bookLib.showBookLib(bookLib.findBook(matchBook,num),num);
+
+  cout<<"\n请选择操作:\n"<<"g) 插入图书       q)退出\n";
+   while (cin.get()!='q')
+   {cin.get();
+    bookLib.insertBook();
+    cout<<"\n请选择操作:\n"<<"g) 插入图书       q)退出\n";
+   }
+    bookLib.showBookLib();
+
+    return 0;
+}
